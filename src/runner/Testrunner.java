c29bd61d681fe2633e1ca38d160f2004f00375a4
Arunakarella <arunakkarella@gmail.com>
2022-01-19 09:56:16 +0400

    second change update

--- src/runner/Testrunner.java
+++ src/runner/Testrunner.java
@@ -1,5 +1,15 @@
 package runner;
 
+import org.testng.annotations.Test;
+
 public class Testrunner {
 
+	@Test 
+	public void postjira() {
+		System.out.println("postJira");
+	    System.out.println("Postjira2");
+	    System.out.println("Post jira3");
+	    
+	
+	}
 }
